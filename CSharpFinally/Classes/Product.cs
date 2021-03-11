c3637db82d04402e62bc458c7ed233821ef23aba
muradkerimov <77536850+muradkerimov@users.noreply.github.com>
2021-03-11 17:09:34 +0400

    prop are added in product class

--- CSharpFinally/Classes/Product.cs
+++ CSharpFinally/Classes/Product.cs
@@ -8,5 +8,16 @@ namespace CSharpFinally.Classes
 {
     class Product
     {
+        public enum Catecory { }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Number { get; set; }
+        public readonly int Id;
+        private static int _id = 0;
+        public Product()
+        {
+            _id++;
+            Id = _id;
+        }
     }
 }
