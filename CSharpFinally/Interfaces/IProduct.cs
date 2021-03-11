3e78948dff00750f770e1e76d0db8c720ff731c2
muradkerimov <77536850+muradkerimov@users.noreply.github.com>
2021-03-11 17:47:21 +0400

    Methods are added to Interfaces

--- CSharpFinally/Interfaces/IProduct.cs
+++ CSharpFinally/Interfaces/IProduct.cs
@@ -8,5 +8,19 @@ namespace CSharpFinally.Interfaces
 {
     interface IProduct
     {
+        //Mehsullar
+        void Products();
+        //Satisdan mehsulun geri qaytarilmasi
+        string ProductSales();
+        //Yeni mehsul elave etmek
+        void AddProducts();
+        //Mehsulun adini,sayini ve meblegini,categoriyasini deyismek
+        void ChangeProductsInfo();
+        //Verilmis kateqoriyaya esasen hemin kateqoriyada olan mehsullarin qaytarilmasi
+        string ProductForCatecory();
+        //Verilmis qiymet araligina esasen hemin araliqda olan mehsullarin qaytarilmasi
+        string ProductForPrice();
+        //Verilmis ada esasen mehsullarin search edilib qaytarilmasi
+        string ShowProductForName();
     }
 }
--- CSharpFinally/Interfaces/ISales.cs
+++ CSharpFinally/Interfaces/ISales.cs
@@ -8,5 +8,19 @@ namespace CSharpFinally.Interfaces
 {
     interface ISales
     {
+        //Satislar
+        void Sales();
+        //Satis elave etmek
+        string AddSales();
+        //Umumi satisin geri qaytarilmasi
+        void AllSales();
+        //Verilen tarix araligina gore hemin tarix araligina olan satislarin qaytarilmasi
+        string SalesForData();
+        //Verilen bir tarixe gore hemin tarix (il,ay,gun) ucun olan satislarin qaytarilmasi
+        string SalesForExactData();
+        //Verilmis mebleg araligina gore edilmis satislarin qaytarilmasi
+        string SalesForPrice();
+        //Verilmis nomreye esasen satisin qaytarilmasi
+        string SalesForNumber();
     }
 }
